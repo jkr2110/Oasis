7eb9071f95b4f92b9334a1eceee2a7df958c8104
Warren Reed <warren.reed@gro-ventures.com>
2014-03-03 15:50:41 -0500

    updating procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn fooddesert.wsgi
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
--- fooddesert/settings.py
+++ fooddesert/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'oasis',
     'bootstrap_toolkit',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
